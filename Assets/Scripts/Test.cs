a6fd2103d497534b3c83eba909e2be323d294408
zhangxinlin <429350061@qq.com>
2021-09-08 11:15:42 +0800

    开发提交

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Linq;
 
 public class Test : MonoBehaviour
 {
+    public Transform[] path;
     // Start is called before the first frame update
     void Start()
     {
+        Text tx = transform.GetComponent<Text>();
+        //tx.DOText("送到发送到",2).SetEase(Ease.Linear).OnComplete(()=> {
+        //    Debug.Log("CCCCCCCCCCCCCCCCC");
+        //});
+
+        //tx.DOText("送到发送到", 2).SetEase(Ease.Linear).OnComplete(CompleteCall);
+        var posList = path.Select(u => u.position).ToArray();
+        Debug.Log("CCCCCCCCCCCCCCCCC   "+ posList.Length);
+        transform.DOPath(posList, 5, PathType.CatmullRom, PathMode.Full3D, 10, Color.red);
         
     }
+    void CompleteCall()
+    {
+        Debug.Log("CCCCCCCCCCCCCCCCC");
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.DOLocalMove(Vector3.left, 2);
+        //transform.DOPunchPosition()
     }
 }
